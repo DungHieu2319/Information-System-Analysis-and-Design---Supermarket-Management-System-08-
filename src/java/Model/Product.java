46257e9735af78f46387c4dcf6f516d83d71a522
DungHieu2319 <dungtrantien2005@gmail.com>
2025-11-11 11:18:47 +0700

    Modified Product + User

--- src/java/Model/Product.java
+++ src/java/Model/Product.java
@@ -9,5 +9,74 @@ package Model;
  * @author Tran Tien Dung - B22DCAT054
  */
 public class Product {
+    private int id;
+    private String code;
+    private String name;
+    private String category;
+    private float price;
+    private float stockQuantity;
+    private String description;
+
+    public Product() {
+    }
+
+    public Product(int id, String code, String name, String category, float price, float stockQuantity, String description) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public float getStockQuantity() {
+        return stockQuantity;
+    }
+    public void setStockQuantity(float stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
     
 }
--- src/java/Model/User.java
+++ src/java/Model/User.java
@@ -4,10 +4,108 @@
  */
 package Model;
 
+import java.util.Date;
+
 /**
  *
  * @author Tran Tien Dung - B22DCAT054
  */
 public class User {
+    private int id;
+    private String code;
+    private String name;
+    private String username;
+    private String password;
+    private Date dateOfBirth;
+    private String address;
+    private String email;
+    private String phoneNumber;
+    private String role;
+
+    public User() {
+    }
+
+    public User(int id, String code, String name, String username, String password, Date dateOfBirth, String address, String email, String phoneNumber, String role) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
     
 }
